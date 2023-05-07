d32971600c8ae788c60dd5a9d91938638443dd95
Oleksandr <OlexandrTanasienko@gmail.com>
2023-05-07 12:13:35 +0300

    Create empty class for merge sort

---
+++ ParallelProgrammingCourseWork/ParallelProgrammingCourseWork/SequentialMergeSorter.cs
@@ -0,0 +1,9 @@
+﻿namespace ParallelProgrammingCourseWork;
+
+public class SequentialMergeSorter : ISorter
+{
+    public int[] Sort(int[] array)
+    {
+        throw new NotImplementedException();
+    }
+}
