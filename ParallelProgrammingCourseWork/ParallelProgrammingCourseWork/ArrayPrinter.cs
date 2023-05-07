f7631d355743e5bc49772473d923384153f9d208
Oleksandr <OlexandrTanasienko@gmail.com>
2023-05-07 12:14:45 +0300

    Add simple function for array printing

---
+++ ParallelProgrammingCourseWork/ParallelProgrammingCourseWork/ArrayPrinter.cs
@@ -0,0 +1,12 @@
+﻿namespace ParallelProgrammingCourseWork;
+
+public static class ArrayPrinter
+{
+    public static void PrintArray(int[] array)
+    {
+        foreach (var element in array)
+            Console.Write($"{element} ");
+
+        Console.WriteLine();
+    }
+}
