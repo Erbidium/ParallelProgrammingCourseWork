722763b8932005093a8ab733b05f978220e9fd50
Oleksandr <OlexandrTanasienko@gmail.com>
2023-05-07 15:42:00 +0300

    Split merge sorters to separate classes

---
+++ ParallelProgrammingCourseWork/ParallelProgrammingCourseWork/ParallelForMergeSorter.cs
@@ -0,0 +1,33 @@
+﻿namespace ParallelProgrammingCourseWork;
+
+public class ParallelForMergeSorter : ISorter
+{
+    public void Sort(int[] array)
+    {
+        ParallelForMergeSort(array, 0, array.Length - 1);
+    }
+    
+    private static void ParallelForMergeSort(int[] array, int leftIndex, int rightIndex)
+    {
+        if (leftIndex >= rightIndex) return;
+        
+        var middlePoint = (leftIndex + rightIndex) / 2;
+
+        var leftIndices = new [] { leftIndex, middlePoint + 1 };
+        var rightIndices = new[] { middlePoint + 1, rightIndex };
+
+        Parallel.For(0, 2, index =>
+        {
+            if (rightIndices[index] - leftIndices[index] > 4000)
+            {
+                ParallelForMergeSort(array, leftIndices[index], rightIndices[index]);
+            }
+            else
+            {
+                SequentialMergeSorter.MergeSort(array, middlePoint + 1, rightIndex);
+            }
+        });
+
+        SequentialMergeSorter.Merge(array, leftIndex, middlePoint, rightIndex);
+    }
+}
---
+++ ParallelProgrammingCourseWork/ParallelProgrammingCourseWork/ParallelTaskMergeSorter.cs
@@ -0,0 +1,44 @@
+﻿namespace ParallelProgrammingCourseWork;
+
+public class ParallelTaskMergeSorter : ISorter
+{
+    public void Sort(int[] array)
+    {
+        ParallelTaskMergeSort(array, 0, array.Length - 1);
+    }
+    
+    private static void ParallelTaskMergeSort(int[] array, int leftIndex, int rightIndex)
+    {
+        if (leftIndex >= rightIndex) return;
+        
+        var middlePoint = (leftIndex + rightIndex) / 2;
+            
+        var task1 = Task.Run(() =>
+        {
+            if (middlePoint - leftIndex > 4000)
+            {
+                ParallelTaskMergeSort(array, leftIndex, middlePoint);
+            }
+            else
+            {
+                SequentialMergeSorter.MergeSort(array, leftIndex, middlePoint);
+            }
+        });
+        var task2 = Task.Run(() =>
+        {
+            if (rightIndex - middlePoint - 1 > 4000)
+            {
+                ParallelTaskMergeSort(array, middlePoint + 1, rightIndex);
+            }
+            else
+            {
+                SequentialMergeSorter.MergeSort(array, middlePoint + 1, rightIndex);
+            }
+            
+        });
+
+        Task.WaitAll(task1, task2);
+
+        SequentialMergeSorter.Merge(array, leftIndex, middlePoint, rightIndex);
+    }
+}
