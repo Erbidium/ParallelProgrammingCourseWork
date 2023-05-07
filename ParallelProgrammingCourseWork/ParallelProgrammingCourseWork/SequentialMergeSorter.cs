571ad5ebf9886aea63f47d78eda556e0370f71b4
Oleksandr <OlexandrTanasienko@gmail.com>
2023-05-07 12:50:06 +0300

    Implement sequential merge sort

--- ParallelProgrammingCourseWork/ParallelProgrammingCourseWork/SequentialMergeSorter.cs
+++ ParallelProgrammingCourseWork/ParallelProgrammingCourseWork/SequentialMergeSorter.cs
@@ -2,8 +2,60 @@
 
 public class SequentialMergeSorter : ISorter
 {
-    public int[] Sort(int[] array)
+    public void Sort(int[] array)
     {
-        throw new NotImplementedException();
+        MergeSort(array, 0, array.Length - 1);
+    }
+    
+    private static void MergeSort(int[] array, int leftIndex, int rightIndex)
+    {
+        if (leftIndex >= rightIndex) return;
+        
+        var middlePoint = (leftIndex + rightIndex) / 2;
+            
+        MergeSort(array, leftIndex, middlePoint);
+        MergeSort(array, middlePoint + 1, rightIndex);
+
+        Merge(array, leftIndex, middlePoint, rightIndex);
+    }
+    
+    private static void Merge(int[] array, int leftIndex, int middlePoint, int rightIndex)
+    {
+        int firstSubArraySize = middlePoint - leftIndex + 1;
+        int secondSubArraySize = rightIndex - middlePoint;
+
+        var firstSubArrayTemp = new int[firstSubArraySize];
+        var secondSubArrayTemp = new int[secondSubArraySize];
+
+        Array.Copy(array, leftIndex, firstSubArrayTemp, 0, firstSubArraySize);
+        Array.Copy(array, middlePoint + 1, secondSubArrayTemp, 0, secondSubArraySize);
+        
+        int firstSubArrayIndex = 0;
+        int secondSubArrayIndex = 0;
+
+        int mergedSubArrayIndex = leftIndex;
+        while (firstSubArrayIndex < firstSubArraySize && secondSubArrayIndex < secondSubArraySize) {
+            if (firstSubArrayTemp[firstSubArrayIndex] <= secondSubArrayTemp[secondSubArrayIndex]) {
+                array[mergedSubArrayIndex] = firstSubArrayTemp[firstSubArrayIndex];
+                firstSubArrayIndex++;
+            }
+            else {
+                array[mergedSubArrayIndex] = secondSubArrayTemp[secondSubArrayIndex];
+                secondSubArrayIndex++;
+            }
+            mergedSubArrayIndex++;
+        }
+        
+        while (firstSubArrayIndex < firstSubArraySize) {
+            array[mergedSubArrayIndex] = firstSubArrayTemp[firstSubArrayIndex];
+            firstSubArrayIndex++;
+            mergedSubArrayIndex++;
+        }
+        
+        while (secondSubArrayIndex < secondSubArraySize) {
+            array[mergedSubArrayIndex] = secondSubArrayTemp[secondSubArrayIndex];
+            secondSubArrayIndex++;
+            mergedSubArrayIndex++;
+        }
     }
 }
