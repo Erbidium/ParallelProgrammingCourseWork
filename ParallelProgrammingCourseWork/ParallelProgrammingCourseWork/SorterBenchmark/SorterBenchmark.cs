ae685613fe984dcb3ea1889ae58e3a03850f7fa2
Oleksandr <OlexandrTanasienko@gmail.com>
2023-05-20 21:48:50 +0300

    Create sorter benchmark

---
+++ ParallelProgrammingCourseWork/ParallelProgrammingCourseWork/SorterBenchmark/SorterBenchmark.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using ParallelProgrammingCourseWork.ArrayHelpers;
+using ParallelProgrammingCourseWork.Interfaces;
+
+namespace ParallelProgrammingCourseWork.SorterBenchmark;
+
+public class SorterBenchmark
+{
+    public void Run(ISorter sorter, int executionTimesCount, int[] array)
+    {
+        for (int i = 0; i < executionTimesCount; i++)
+        {
+            var startTime = Stopwatch.GetTimestamp();
+            
+            sorter.Sort(array);
+
+            var endTime = Stopwatch.GetElapsedTime(startTime);
+            Console.WriteLine(endTime.TotalSeconds);
+    
+            if (!ArrayValidator.ArrayIsSorted(array))
+                Console.WriteLine("Array is not sorted correctly");
+            
+            Console.WriteLine(endTime.TotalMilliseconds);
+        }
+
+        //Console.WriteLine("Sorted array");
+        //ArrayPrinter.PrintArray(array);
+    }
+}
