6b2f939fb554c5eb82db561f452c74e3311a5337
Oleksandr <OlexandrTanasienko@gmail.com>
2023-05-24 01:04:42 +0300

    Add array generators for other types

---
+++ ParallelProgrammingCourseWork/ParallelProgrammingCourseWork/ArrayGenerators/DoubleArrayGenerator.cs
@@ -0,0 +1,19 @@
+﻿using ParallelProgrammingCourseWork.Abstractions;
+
+namespace ParallelProgrammingCourseWork.ArrayGenerators;
+
+public class DoubleArrayGenerator : IArrayGenerator<double>
+{
+    public double[] GenerateArray(int size)
+    {
+        var array = new double[size];
+
+        var random = new Random();
+        for (var i = 0; i < array.Length; i++)
+        {
+            array[i] = random.NextDouble();
+        }
+
+        return array;
+    }
+}
---
+++ ParallelProgrammingCourseWork/ParallelProgrammingCourseWork/ArrayGenerators/StringArrayGenerator.cs
@@ -0,0 +1,28 @@
+﻿using ParallelProgrammingCourseWork.Abstractions;
+
+namespace ParallelProgrammingCourseWork.ArrayGenerators;
+
+public class StringArrayGenerator : IArrayGenerator<string>
+{
+    public string[] GenerateArray(int size)
+    {
+        var stringsArray = new string[size];
+        
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        
+        var stringBuffer = new char[4];
+        var random = new Random();
+        
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < stringBuffer.Length; j++)
+            {
+                stringBuffer[j] = chars[random.Next(chars.Length)];
+            }
+
+            stringsArray[i] = new string(stringBuffer);
+        }
+
+        return stringsArray;
+    }
+}
